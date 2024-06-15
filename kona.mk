90af33de437ed40f8ca388199a4e355be935c5e9
Alcatraz323 <alcatraz32323@gmail.com>
2024-06-15 22:34:47 +0800

    sm8250-common: overlay: reconstruct to full RRO

--- kona.mk
+++ kona.mk
@@ -270,12 +270,14 @@ PRODUCT_COPY_FILES += \
 TARGET_SUPPORTS_OMX_SERVICE := false
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
+PRODUCT_PACKAGES += \
+    CarrierConfigCommonOverlay \
+    FrameworksCommonOverlay \
+    SettingsCommonOverlay \
+    SettingsProviderCommonOverlay \
+    SystemUICommonOverlay \
+    TelephonyCommonOverlay \
+    WifiCommonOverlay
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -372,7 +374,6 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
-    WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
 
