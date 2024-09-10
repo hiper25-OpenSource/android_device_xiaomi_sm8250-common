78c5352d76f2b3796aa6027349a96c3f679b824d
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    sm8250-common: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- kona.mk
+++ kona.mk
@@ -166,7 +166,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libcrypto_shim
 
 # fastbootd
 PRODUCT_PACKAGES += \
