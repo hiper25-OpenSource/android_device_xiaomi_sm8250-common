c7d70e9644df93f9e84d1209ac0901375a59d4af
Alcatraz323 <alcatraz32323@gmail.com>
2024-12-12 21:07:07 +0800

    cmi: Implement DSPVolumeSynchronizer
    * cmi also needs this to handle bass dynamically
    
    Change-Id: I750803d94161e1e7482552d2a39566f42e82fc0a

--- device.mk
+++ device.mk
@@ -231,6 +231,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# DSP Volume Synchronizer
+PRODUCT_PACKAGES += \
+    DSPVolumeSynchronizer
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
