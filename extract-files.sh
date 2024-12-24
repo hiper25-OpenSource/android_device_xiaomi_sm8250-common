b0ea1a6a09ef7ab828e63dbbec6aa8a9a4a8ea38
Yumi Yukimura <me.cafebabe@gmail.com>
2024-12-25 04:40:45 +0800

    sm8250-common: Allow gettid syscall for atfwd@2.0
    Change-Id: I0d7854b73e0b7e822edce89b9a3d6e33e1f45fc6

--- extract-files.sh
+++ extract-files.sh
@@ -87,6 +87,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "/seclabel u:r:mi_thermald:s0/d" "${2}"
             ;;
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            [ "$2" = "" ] && return 0
+            grep -q 'gettid: ' "${2}" || echo 'gettid: 1' >> "${2}"
+            ;;
         vendor/lib64/libril-qc-hal-qmi.so)
             [ "$2" = "" ] && return 0
             sed -i 's|ro.product.vendor.device|ro.vendor.radio.midevice|g' "${2}"
