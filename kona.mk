983edad52320d4c557f3fd8e1fccf75bc5f57b4f
hiper25 <whydonit@gmail.com>
2025-06-21 16:17:38 +0800

    sm8250-common: idc & keylayout: remove unwanted

--- kona.mk
+++ kona.mk
@@ -217,16 +217,9 @@ PRODUCT_PACKAGES += \
 
 # Input
 PRODUCT_PACKAGES += \
-    Xiaomi_Smart_Pen_Keyboard.kl \
-    kona-mtp-snd-card_Button_Jack.kl \
-    uinput-fortsense.kl \
-    uinput-fpc.kl \
-    uinput-goodix.kl
+    kona-mtp-snd-card_Button_Jack.kl
 
 PRODUCT_PACKAGES += \
-    Xiaomi_Keyboard.idc \
-    uinput-fortsense.idc \
-    uinput-fpc.idc \
     uinput-goodix.idc
 
 # Kernel
